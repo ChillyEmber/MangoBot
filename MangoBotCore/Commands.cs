7171234e4092d6c875f66a7a704c7ede9c88c582
Seth Feuer <lXxMangoxXl@gmail.com>
2020-10-08 11:49:08 -0400

    Allowed text to be put into 8ball command

--- MangoBotCore/Commands.cs
+++ MangoBotCore/Commands.cs
@@ -137,7 +137,7 @@ namespace MangoBotCommandsNamespace
 
 
         [Command("8ball")]
-        private async Task eightball()
+        private async Task eightball(params string[] args)
         {
             string[] eightballquotes = { "As I see it, yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don’t count on it.", "Don’t count on it.", "It is certain.",
                 "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes", "Very doubtful", "Without a doubt.",
